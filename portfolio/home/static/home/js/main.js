dc11c67f30fa124924d744224022aa74f72355a9
MarcoB <brigomarco11@gmail.com>
2024-09-24 17:34:44 +0200

    update changes on style

--- portfolio/home/static/home/js/main.js
+++ portfolio/home/static/home/js/main.js
@@ -1,14 +1,15 @@
 import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.module.js";
 import { OrbitControls } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js";
 import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
+import { DRACOLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/DRACOLoader.js";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 500);
-const initial_position = new THREE.Vector3(5, 7, 8);
+const initial_position = new THREE.Vector3(5, 7, 8); //5,7,8
 camera.position.copy(initial_position);
 
 const verify_aspect = window.innerWidth / window.innerHeight;
-console.log(verify_aspect);
+//console.log(verify_aspect);
 if(verify_aspect < 1){ 
     camera.position.z=(initial_position.z / verify_aspect);
     if(verify_aspect < 0.6) camera.position.z -= 3;
@@ -24,22 +25,40 @@ let controls = new OrbitControls(camera, renderer.domElement);
 controls.enableZoom = false;
 controls.enablePan = false;
 
+//Loaders
 const loader = new GLTFLoader();
 const url = '/static/home/models/commodore_minimal/scene.gltf';
 
-const ambientLight = new THREE.AmbientLight(0x404040); // Soft white light
+const dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath('https://www.gstatic.com/draco/v1/decoders/'); 
+loader.setDRACOLoader( dracoLoader );
+
+
+//Lights
+const ambientLight = new THREE.AmbientLight(0xffe0b0, 0.3); // Soft white light
 scene.add(ambientLight);
 
 const directionalLight = new THREE.DirectionalLight(0xFFFFFF, 1);
-directionalLight.position.set(5, 5, 5).normalize();  // Position the light
+directionalLight.position.set(5, 5, 5).normalize();  // Position the light 5,5,5
 scene.add(directionalLight);
 
-let loadedModel;
 
-loader.load(url, function(gltf) {
+//Load model and set animations
+let loadedModel;
+let mixer; 
+
+loader.load(url, function (gltf) {
     loadedModel = gltf.scene;
     scene.add(gltf.scene);
-}, undefined, function(error) {
+
+    //loadedModel.scale.set(2,2,2);
+
+    if (gltf.animations && gltf.animations.length > 0) {
+        mixer = new THREE.AnimationMixer(gltf.scene); 
+        const action = mixer.clipAction(gltf.animations[0]); 
+        action.play(); 
+    }
+}, undefined, function (error) {
     console.error('An error occurred', error);
 });
 
@@ -48,11 +67,18 @@ function animate(){
 
     if (loadedModel) {
         loadedModel.rotation.y += 0.01;  
+        //console.log("Camera Position: ", camera.position);
     }
 
+    if (mixer) {
+        mixer.update(0.01); 
+    }
+
+    directionalLight.lookAt(camera.position);
     controls.update();
     renderer.render(scene,camera);
 }
+
 
 window.addEventListener("resize",function(){
     const containerWidth = container.offsetWidth;
---
+++ portfolio/home/static/home/js/type_animation.js
@@ -0,0 +1,11 @@
+// var typed = new Typed('.h1_title', {
+//     strings: ["Hello,<br> I'm <strong>Marco Brigo</strong>"],
+//     typeSpeed: 80,
+//   });
+
+var typed = new Typed('.h1_title', {
+    stringsElement: '.typed-text',
+    startDelay: 1000,
+    typeSpeed: 80,
+    smartBackspace: false,
+  });
